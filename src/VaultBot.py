f7cbc6441a2dbf4e1e1ecab11aeea33754bd930e
Sandro Schnetzer <crim5on@protonmail.com>
2025-01-27 13:07:34 +0100

    bot now answers with the according lookup from dictionary.

--- src/VaultBot.py
+++ src/VaultBot.py
@@ -3,7 +3,7 @@
 ##
 # file: VaultBot.py
 #
-# date: 2025-01-26
+# date: 2025-01-27
 # author: Sandro Schnetzer
 # contact: https://www.linkedin.com/in/sandroschnetzer/
 # libraries: https://python-telegram-bot.org/
@@ -25,42 +25,62 @@ from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandl
 
 # Enable logging
 logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
-# set higher logging level for httpx to avoid all GET and POST requests being logged
 logging.getLogger("httpx").setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
 
 
 
-def load_config(file: str) -> dict:     # TODO: Is it a dict:
-    with open(file) as configfile:
-        config = json.load(configfile)
-        return config
+def load_json(file: str) -> dict:
+    with open(file) as filestream:
+        json_dict = json.load(filestream)
+        return json_dict
 
 def read_token(file: str) -> str:
     with open(file) as tokenfile: 
         token = tokenfile.read()
         return token
 
-# Define a few command handlers. These usually take the two arguments update and context.
 
 
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(update.message.text + " - VaultBot")
+
+def extract_words(sentence: str) -> list:
+    words = sentence.split()
+    return words
+
+def clean_word(word: str) -> str:
+    clean_word = ''.join(filter(str.isalnum, word))       # remove non-alphanumeric chars
+    clean_word = clean_word.lower()
+    return clean_word
+
+def get_answer_from_keyword(dictionary: dict, sentence: str) -> str:
+    words = extract_words(sentence)
+    for word in words:  # TODO: check words in sentence from the back.
+        answer = dictionary.get(clean_word(word), None)
+        if answer is not None:
+            return answer
+    return None
+
+
+
+# command handler
+async def dict_reply(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    answer = get_answer_from_keyword(dictionary, update.message.text)
+    if answer is not None:
+        await update.message.reply_text(answer)
+
+
+
+
 
 
 def main() -> None:
-
-    config = load_config("./config.json")
-    print("token: " + config["tokenfile"])
+    config = load_json("./config.json")
     token = read_token(config["tokenfile"])
+    global dictionary
+    dictionary = load_json(config["dictionary"])    #TODO: pass through
 
-    # Start the bot.Create the Application and pass it your bot's token.
     application = Application.builder().token(token).build()
-
-    # on non command i.e message - echo the message on Telegram
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
-
-    # Run the bot until the user presses Ctrl-C
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, dict_reply))
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
 
